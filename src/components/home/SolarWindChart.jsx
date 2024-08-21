bf9c0618e999a0327bea752a5f6df4bfc0c3c5f1
Srejon Khan <srejonkhan79@gmail.com>
2024-08-22 03:56:55 +0600

    feat: update solar wind data

--- src/components/home/SolarWindChart.jsx
+++ src/components/home/SolarWindChart.jsx
@@ -1,38 +1,51 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {
-  ScatterChart,
-  Scatter,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ZAxis } from "recharts";
 
 const SolarWindChart = () => {
-  const [data, setData] = useState([]);
+  const [times, setTimes] = useState([]);
+  const [bxGsm, setBxGsm] = useState([]);
+  const [byGsm, setByGsm] = useState([]);
+  const [bzGsm, setBzGsm] = useState([]);
+  const [bt, setBt] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(
-          "https://services.swpc.noaa.gov/products/solar-wind/mag-1-day.json"
-        );
+        const res = await axios.get("https://services.swpc.noaa.gov/products/solar-wind/mag-1-day.json");
         if (res.status === 200) {
-          const formattedData = res.data.slice(1).map((item) => {
-            return {
-              time_tag: new Date(item[0]).toLocaleString("sv-SE", {
-                timeZone: "UTC",
-              }),
-              bx_gsm: item[1],
-              by_gsm: item[2],
-              bz_gsm: item[3],
-              bt: item[6],
-            };
+          const timeData = [];
+          const bxGsmData = [];
+          const byGsmData = [];
+          const bzGsmData = [];
+          const btData = [];
+          res.data.slice(1).forEach((item) => {
+            // bxGsmData.push({ x: item[6], y: item[1] });
+            // byGsmData.push({ x: item[6], y: item[2] });
+            // bzGsmData.push({ x: item[6], y: item[3] });
+            timeData.push(item[0]);
+            bxGsmData.push({ x: item[0], y: Number(item[1]) });
+            byGsmData.push({ x: item[0], y: Number(item[2]) });
+            bzGsmData.push({ x: item[0], y: Number(item[3]) });
+            btData.push({ x: item[0], y: Number(item[6]) });
           });
-          setData(formattedData);
+          setTimes(timeData);
+          setBxGsm(bxGsmData);
+          setByGsm(byGsmData);
+          setBzGsm(bzGsmData);
+          setBt(btData);
+          // const formattedData = res.data.slice(1).map((item) => {
+          //   return {
+          //     time_tag: new Date(item[0]).toLocaleString("sv-SE", {
+          //       timeZone: "UTC",
+          //     }),
+          //     bx_gsm: item[1],
+          //     by_gsm: item[2],
+          //     bz_gsm: item[3],
+          //     bt: item[6],
+          //   };
+          // });
+          // setData(formattedData);
         }
       } catch (error) {
         console.log(error);
@@ -43,11 +56,34 @@ const SolarWindChart = () => {
 
   return (
     <div>
-      <div className="flex items-center justify-between px-6">
-        <h4>{data.length > 0 && data[0].time_tag}</h4>
-        <h4>1 day @1 min</h4>
-        <h4>{data.length > 0 && data[data.length - 1].time_tag}</h4>
+      <div>
+        <h3 className="text-3xl font-bold px-6">Bx Data</h3>
+        <div className="flex items-center justify-between px-6">
+          <h4>{times.length > 0 && times[0]}</h4>
+          <h4>1 day @1 min</h4>
+          <h4>{times.length > 0 && times[times.length - 1]}</h4>
+        </div>
+        <ResponsiveContainer width="100%" height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="x" name="Time" tick={false} />
+            <YAxis dataKey="y" />
+            {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
+            <ZAxis range={[25, 25]} />
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Legend />
+            <Scatter name="Bx GSM" data={bxGsm} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
       </div>
+
       <ResponsiveContainer width="100%" height={400}>
         <ScatterChart
           margin={{
@@ -58,32 +94,54 @@ const SolarWindChart = () => {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time_tag" name="Time" type="category" />
-          <YAxis domain={[-10, 10]} />
+          <XAxis dataKey="x" name="Time" />
+          <YAxis dataKey="y" />
+          <ZAxis range={[20, 20]} />
+          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
           <Tooltip cursor={{ strokeDasharray: "3 3" }} />
           <Legend />
-          <Scatter
-            name="Bx GSM"
-            data={data}
-            dataKey="bx_gsm"
-            fill="#8884d8"
-            line
-          />
-          <Scatter
-            name="By GSM"
-            data={data}
-            dataKey="by_gsm"
-            fill="#82ca9d"
-            line
-          />
-          <Scatter
-            name="Bz GSM"
-            data={data}
-            dataKey="bz_gsm"
-            fill="#ffc658"
-            line
-          />
+          <Scatter name="By GSM" data={byGsm} fill="#82ca9d" />
+        </ScatterChart>
+      </ResponsiveContainer>
+
+      <ResponsiveContainer width="100%" height={400}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 10,
+            left: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="x" name="Time" />
+          <YAxis dataKey="y" />
+          <ZAxis range={[20, 20]} />
+          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Legend />
+          <Scatter name="Bz GSM" data={bzGsm} fill="#ffc658" />
           {/* <Scatter name="BT" data={data} dataKey="bt" fill="#ff7300" line /> */}
+        </ScatterChart>
+      </ResponsiveContainer>
+
+      <ResponsiveContainer width="100%" height={400}>
+        <ScatterChart
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 10,
+            left: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="x" name="Time" />
+          <YAxis dataKey="y" />
+          <ZAxis range={[20, 20]} />
+          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Legend />
+          <Scatter name="Bt" data={bt} fill="#ff7300" />
         </ScatterChart>
       </ResponsiveContainer>
     </div>
