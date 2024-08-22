96dfc0be54c36f65877fbfd8c8c2cc63b73d1a1a
Srejon Khan <srejonkhan79@gmail.com>
2024-08-22 16:00:47 +0600

    feat: update solar wind chart

--- src/components/home/SolarWindChart.jsx
+++ src/components/home/SolarWindChart.jsx
@@ -56,8 +56,71 @@ const SolarWindChart = () => {
 
   return (
     <div>
+      <h2 className="text-3xl font-bold px-6">Realtime Solar Wind</h2>
+
+      {/* Bx Data */}
       <div>
-        <h3 className="text-3xl font-bold px-6">Bx Data</h3>
+        <h3 className="text-xl font-bold px-6">Bx</h3>
+        <div className="flex items-center justify-between px-6">
+          <h4>{times.length > 0 && times[0]}</h4>
+          <h4>1 day @1 min</h4>
+          <h4>{times.length > 0 && times[times.length - 1]}</h4>
+        </div>
+        <ResponsiveContainer width="100%" height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+
+            <XAxis dataKey="x" name="Time" tick={false} />
+            <YAxis dataKey="y" />
+            <ZAxis range={[25, 25]} />
+
+            <Tooltip cursor={{ strokeDasharray: "3 3", strokeWidth: 2 }} />
+            <Legend />
+            <Scatter name="Bx GSM" data={bxGsm} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </div>
+
+      {/* By Data */}
+      <div>
+        <h3 className="text-xl font-bold px-6">By</h3>
+        <div className="flex items-center justify-between px-6">
+          <h4>{times.length > 0 && times[0]}</h4>
+          <h4>1 day @1 min</h4>
+          <h4>{times.length > 0 && times[times.length - 1]}</h4>
+        </div>
+        <ResponsiveContainer width="100%" height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+
+            <XAxis dataKey="x" name="Time" />
+            <YAxis dataKey="y" />
+            <ZAxis range={[20, 20]} />
+
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Legend />
+            <Scatter name="By GSM" data={byGsm} fill="#82ca9d" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </div>
+
+      {/* Bz GSM Data */}
+      <div>
+        <h3 className="text-xl font-bold px-6">Bz GSM(nT)</h3>
         <div className="flex items-center justify-between px-6">
           <h4>{times.length > 0 && times[0]}</h4>
           <h4>1 day @1 min</h4>
@@ -75,75 +138,44 @@ const SolarWindChart = () => {
             <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey="x" name="Time" tick={false} />
             <YAxis dataKey="y" />
-            {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
-            <ZAxis range={[25, 25]} />
+            <ZAxis range={[20, 20]} />
+
             <Tooltip cursor={{ strokeDasharray: "3 3" }} />
             <Legend />
-            <Scatter name="Bx GSM" data={bxGsm} fill="#8884d8" />
+            <Scatter name="Bz GSM" data={bzGsm} fill="#ffc658" />
+            {/* <Scatter name="BT" data={data} dataKey="bt" fill="#ff7300" line /> */}
           </ScatterChart>
         </ResponsiveContainer>
       </div>
 
-      <ResponsiveContainer width="100%" height={400}>
-        <ScatterChart
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 10,
-            left: 10,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" name="Time" />
-          <YAxis dataKey="y" />
-          <ZAxis range={[20, 20]} />
-          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Legend />
-          <Scatter name="By GSM" data={byGsm} fill="#82ca9d" />
-        </ScatterChart>
-      </ResponsiveContainer>
+      {/* Bt Graph */}
+      <div>
+        <h3 className="text-xl font-bold px-6">Bt</h3>
+        <div className="flex items-center justify-between px-6">
+          <h4>{times.length > 0 && times[0]}</h4>
+          <h4>1 day @1 min</h4>
+          <h4>{times.length > 0 && times[times.length - 1]}</h4>
+        </div>
+        <ResponsiveContainer width="100%" height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 10,
+              left: 10,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="x" name="Time" tick={false} />
+            <YAxis dataKey="y" />
+            <ZAxis range={[20, 20]} />
 
-      <ResponsiveContainer width="100%" height={400}>
-        <ScatterChart
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 10,
-            left: 10,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" name="Time" />
-          <YAxis dataKey="y" />
-          <ZAxis range={[20, 20]} />
-          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Legend />
-          <Scatter name="Bz GSM" data={bzGsm} fill="#ffc658" />
-          {/* <Scatter name="BT" data={data} dataKey="bt" fill="#ff7300" line /> */}
-        </ScatterChart>
-      </ResponsiveContainer>
-
-      <ResponsiveContainer width="100%" height={400}>
-        <ScatterChart
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 10,
-            left: 10,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="x" name="Time" />
-          <YAxis dataKey="y" />
-          <ZAxis range={[20, 20]} />
-          {/* <ZAxis dataKey="by_gsm" domain={[-10, 10]} /> */}
-          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-          <Legend />
-          <Scatter name="Bt" data={bt} fill="#ff7300" />
-        </ScatterChart>
-      </ResponsiveContainer>
+            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+            <Legend />
+            <Scatter name="Bt" data={bt} fill="#ff7300" />
+          </ScatterChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
